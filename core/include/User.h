026f5063ff9cc3e3593dc62bb7acd3192718a5a3
NazimBekhat <abderrahmane-nazim.bekhat@ensia.edu.dz>
2026-06-13 19:14:04 +0100

    User class header added

---
+++ core/include/User.h
@@ -0,0 +1,24 @@
+#ifndef USER_H
+#define USER_H
+
+#include <iostream>
+using namespace std;
+
+class User{
+    private:
+        string username;
+        string password;
+    public:
+        User(const string& username, const string& password);
+        virtual ~User()=default; //so when we delete a list of pointers of type user it uses the correct destructor of the object
+
+        virtual bool login(const string& inputPassword); //virtual
+        virtual string getRole()=0; //pure virtual
+
+        void setUsername(const string& username);
+        string getUsername() const;
+        void setPassword(const string& password);
+        string getPassword() const;
+};
+
+#endif
